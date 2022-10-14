9d2508bd39c67f48326b0956dc69aff0ced1168b
Carol Kang <otherhats@gmail.com>
2022-10-14 00:03:59 -0400

    add navbar

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Resume from './pages/Resume'
+import Projects from './pages/Projects'
+import NavBar from './components/NavBar'
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
+        <Route path='resume' element={<Resume />} />
+        <Route path='projects' element={<Projects />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
+
