86777c89bad09aaf2a8efb0a9701b23a5532dca8
kangcarol <otherhats@gmail.com>
2022-10-19 20:01:46 -0400

    add project array

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -2,12 +2,23 @@ const projectList = [
   {
     title: 'Tapestry',
     description: 'App designed for contemplation, genuine interactions, to share and learn about others, built with Mongoose, MongoDB, Express, Embedded Javascript, CSS',
-    image: '',
+    image: 'https://imgur.com/x69898z',
     repositoryLink: 'https://github.com/kangcarol/tapestry',
     deploymentLink: 'https://carolkang-tapestry-app.fly.dev/',
   },
   {
-
+    title: 'The Phoenix',
+    description: 'Choose Your Own Adventure game based on the mythological Phoenix, built with Javascript, HTML, CSS',
+    image: 'https://imgur.com/PHWDaM1',
+    repositoryLink: 'https://github.com/kangcarol/choose-your-own-adventure-the-phoenix',
+    deploymentLink: 'https://carol-kang-cyoa-the-phoenix.netlify.app/',
+  },
+  {
+    title: 'Tic Tac Toe',
+    description: 'A classic game in a graphic color scheme and animation, built with Javascript, HTML, CSS',
+    image: 'https://imgur.com/x69898z',
+    repositoryLink: 'https://github.com/kangcarol/ttt-weekend',
+    deploymentLink: 'https://carol-kang-tic-tac-toe.netlify.app/',
   }
 ]
 
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,7 +2,7 @@ function Resume() {
   return (
     <>
       <h1>Resume</h1>
-      <h2>Click to download <a href="/Carol_Kang_Resume_2022.pdf">my resume</a></h2>
+      <h2>Click to download <a href="/Carol_Kang_Resume_2022.pdf" target="_blank" rel="noreferrer">my resume</a></h2>
     </>
   )
 }
