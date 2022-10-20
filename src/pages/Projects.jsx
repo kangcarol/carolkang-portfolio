7577153cc26af222bead0dabeb4b1ac945846043
kangcarol <otherhats@gmail.com>
2022-10-19 23:09:57 -0400

    correct project array urls and home page name

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,22 @@
-import { projectList } from "../data/projects"
+import { projects } from "../data/projects"
 
 function Projects() {
+
   return (
+    <>
       <h1>Projects</h1>
+      <ul>
+			{projects.map((project, idx) =>
+				<li>
+          <Projects
+            key={idx}
+            idx={idx}
+            projectTitle={project.title}
+				  />
+        </li>
+			)}
+      </ul>
+    </>
   )
 }
 
