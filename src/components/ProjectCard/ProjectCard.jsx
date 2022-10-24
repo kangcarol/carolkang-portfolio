6e02ec8220879e47483bee279dde9e2bb06d71a7
kangcarol <otherhats@gmail.com>
2022-10-23 21:52:49 -0400

    redirect

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ProjectCard = ({ project }) => {
   return (
     <>
       <h2>{project.title}</h2>
+      <p>{project.description}</p>
       <img height='300' src={project.image} alt=""/>
+      <button >Learn More</button>
+      <button >View App</button>
     </>
   );
 }
