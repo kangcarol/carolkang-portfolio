dbdbc4c7421c7399635ba9585e00876562b334fa
kangcarol <otherhats@gmail.com>
2022-11-13 00:36:22 -0500

    temp background, add var for text color

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,9 +1,8 @@
-import { Link } from "react-router-dom"
+// import { Link } from "react-router-dom"
 import styles from './Contact.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser,faEnvelope, faFile, faMagnifyingGlass, faCode} from '@fortawesome/free-solid-svg-icons'
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { faReact, faSquareJs, faHtml5, faCss3Alt  } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 function Contact() {
   return (
@@ -15,23 +14,19 @@ function Contact() {
 
           <article>
 
-            {/* <div> */}
               <a href="mailto:carolkang@me.com" target="_blank" rel="noreferrer" alt="Carol Kang email">
                 <span className={styles.contact}>
                   <p><FontAwesomeIcon icon={faEnvelope}/></p>
                   <p>Email</p>
                 </span>
               </a>
-              {/* </div>
 
-              <div> */}
                 <a href="https://www.linkedin.com/in/kang-carol/" target="_blank" rel="noreferrer" alt="Carol Kang LinkedIn">
                   <span className={styles.contact}>
                     <p><FontAwesomeIcon icon={faLinkedin}/></p>
                     <p>LinkedIn</p>
                   </span>
                 </a>
-            {/* </div> */}
 
           </article>
         </section>
