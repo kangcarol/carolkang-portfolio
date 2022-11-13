7bf1a6fa3f6daeca9137bcd0f28de4227d648803
kangcarol <otherhats@gmail.com>
2022-11-12 20:44:32 -0500

    work on responsive css

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -19,37 +19,6 @@ function About() {
           </p>
           </div>
 
-          <article>
-            <a href="/Carol_Kang_Resume_2022.pdf" target="_blank" rel="noreferrer" alt="Carol Kang resume">
-              <span className={styles.resume}>
-                <p><FontAwesomeIcon icon={faEnvelope}/></p>
-                <h2>CV</h2>
-              </span>
-            </a>
-
-            <a href="https://github.com/kangcarol" target="_blank" rel="noreferrer" alt="Carol Kang Github">
-              <span className={styles.contact}>
-                <p><FontAwesomeIcon icon={faGithub}/></p>
-                <h2>GitHub</h2>
-              </span>
-            </a>
-
-              <details className='toolbox'>
-                <summary>TOOL BOX</summary>
-                  <ul>
-                    <li>React</li>
-                    <li>Javascript</li>
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>MongoDB</li>
-                    <li>Mongoose</li>
-                    <li>Express</li>
-                    <li>Node</li>
-                    <li>Django</li>
-                    <li>PostgreSQL</li>
-                  </ul>
-            </details>
-          </article>
         </section>
       </main>
     </>
