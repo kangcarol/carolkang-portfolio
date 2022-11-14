50c97e9219a6f3ba9ea309bcbf3b47931105bc17
kangcarol <otherhats@gmail.com>
2022-11-14 15:28:06 -0500

    refine text in data file

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -1,28 +1,28 @@
 export const projects = [
   {
     title: 'The Botanists Library',
-    description: "The Botanist's Library is a collection of flowers, a place for gathering floral information and to keep track the community care, built with Django, Python, HTML, CSS.",
+    description: "The Botanist's Library is a collection of flowers, a place for gathering floral information and to keep track the community care. Built with Django, Python, HTML, CSS.",
     image: '/p-THE-BOTANISTS-LIBRARY-landing.png',
     repositoryLink: 'https://github.com/kangcarol/the-botanists-library',
     deploymentLink: 'https://the-botanists-library.herokuapp.com/',
   },
   {
     title: 'Tapestry',
-    description: 'App designed for contemplation, genuine interactions, to share and learn about others, built with Mongoose, MongoDB, Express, Embedded Javascript, CSS.',
+    description: 'App designed for contemplation, genuine interactions, to share and learn about others. Built with Mongoose, MongoDB, Express, NodeJS, CSS.',
     image: '/p-TAPESTRY-landing.png',
     repositoryLink: 'https://github.com/kangcarol/tapestry',
     deploymentLink: 'https://carolkang-tapestry-app.fly.dev/',
   },
   {
     title: 'The Phoenix',
-    description: 'Choose Your Own Adventure game based on the mythological Phoenix, built with Javascript, HTML, CSS.   ** Designed for desktop, mouse required to play **',
+    description: 'Choose Your Own Adventure game based on the mythological Phoenix. Built with Javascript, HTML, CSS.   ** Designed for desktop, mouse required to play **',
     image: '/p-THE-PHOENIX-landing.png',
     repositoryLink: 'https://github.com/kangcarol/choose-your-own-adventure-the-phoenix',
     deploymentLink: 'https://carol-kang-cyoa-the-phoenix.netlify.app/',
   },
   {
     title: 'Tic Tac Toe',
-    description: 'A classic game in a graphic color scheme and animation, built with Javascript, HTML, CSS.',
+    description: 'A classic game in a graphic color scheme and animation. Built with Javascript, HTML, CSS.',
     image: '/p-TIC-TAC-TOE-main.png',
     repositoryLink: 'https://github.com/kangcarol/ttt-weekend',
     deploymentLink: 'https://carol-kang-tic-tac-toe.netlify.app/',
