e1c12690bb5d84851a1cce349818d8e32943564d
kangcarol <otherhats@gmail.com>
2022-11-18 13:17:35 -0500

    mobile project list view, adjust, add back the image

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,8 +1,8 @@
 import ProjectList from "../../components/ProjectList"
 import { projects } from "../../data/projects"
 import styles from '../Projects/Projects.module.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass, faCode} from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faMagnifyingGlass, faCode} from '@fortawesome/free-solid-svg-icons'
 
 const Projects = () => {
   return (
